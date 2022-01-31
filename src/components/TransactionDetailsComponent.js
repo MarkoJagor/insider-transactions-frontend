19c293b1212c9cca75d6245a9cc0aa17b6c64772
Marko Jagor <majago@ttu.ee>
2022-01-31 16:50:10 +0200

    add css styling to transaction details component

--- src/components/TransactionDetailsComponent.js
+++ src/components/TransactionDetailsComponent.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Divider } from 'primereact/divider'
 import TransactionService from '../services/TransactionService';
+import '../styles/transactiondetails.css'
 
 const TransactionDetailsComponent = () => {
     const { transactionId } = useParams()
@@ -20,37 +21,41 @@ const TransactionDetailsComponent = () => {
     }
 
     return (
-        <div>
-            <h1>Tehingu detailid</h1>
-            <Divider align='left'>
-                <h3>Emitent</h3>
-            </Divider>
-            <p><b>Nimi:</b> {transaction.issuer}</p>
-            <Divider align='left'>
-                <h3>Juhtimiskohustusi täitva isiku / temaga lähedalt seotud isiku andmed</h3>
-            </Divider>
-            <p><b>Nimi:</b> {transaction.investor}</p>
-            <p><b>Positsioon:</b> {transaction.investorPosition}</p>
-            <Divider align='left'>
-                <h3>Finantsinstrumendi ning tehingu tüüp</h3>
-            </Divider>
-            <p><b>Finantsinstrumendi tüüp:</b> {transaction.instrument}</p>
-            <p><b>Tehingu tüüp:</b> {transaction.transactionType}</p>
-            <Divider align='left'>
-                <h3>Tehingu maht ning väärtus</h3>
-            </Divider>
-            <p><b>Tehingu maht:</b> {transaction.volume}</p>
-            <p><b>Tehingu hind ühe ühiku kohta:</b> {transaction.price}</p>
-            <p><b>Tehingu koondhind:</b> {totalPrice(transaction.volume, transaction.price)}</p>
-            <Divider align='left'>
-                <h3>Muud andmed</h3>
-            </Divider>
-            <p><b>Tehingu kuupäev:</b> {transaction.tradeDate}</p>
-            <p><b>Tehingu avalikustamise kuupäev:</b> {transaction.publishedDate}</p>
-            <p><b>Tehingu koht:</b> {transaction.market}</p>
-            <p><b>Algne teade / muudatus:</b> {transaction.hasBeenUpdated ? 'Muudatus' : 'Algne teade'}</p>
-            {transaction.updateReason !== '' && <p><b>Muudatuse põhjus:</b> {transaction.updateReason}</p>}
-        </div>
+        <main>
+            <h1>Börsiemitendiga seotud isiku tehingu detailid</h1>
+            <div className='bordered-box'>
+                <div className='box-content'>
+                    <Divider align='left'>
+                        <h3>Emitent</h3>
+                    </Divider>
+                    <p><b>Nimi:</b> {transaction.issuer}</p>
+                    <Divider align='left'>
+                        <h3>Juhtimiskohustusi täitva isiku / temaga lähedalt seotud isiku andmed</h3>
+                    </Divider>
+                    <p><b>Nimi:</b> {transaction.investor}</p>
+                    <p><b>Positsioon:</b> {transaction.investorPosition}</p>
+                    <Divider align='left'>
+                        <h3>Finantsinstrumendi ning tehingu tüüp</h3>
+                    </Divider>
+                    <p><b>Finantsinstrumendi tüüp:</b> {transaction.instrument}</p>
+                    <p><b>Tehingu tüüp:</b> {transaction.transactionType}</p>
+                    <Divider align='left'>
+                        <h3>Tehingu maht ning väärtus</h3>
+                    </Divider>
+                    <p><b>Tehingu maht:</b> {transaction.volume}</p>
+                    <p><b>Tehingu hind ühe ühiku kohta:</b> {transaction.price}</p>
+                    <p><b>Tehingu koondhind:</b> {totalPrice(transaction.volume, transaction.price)}</p>
+                    <Divider align='left'>
+                        <h3>Muud andmed</h3>
+                    </Divider>
+                    <p><b>Tehingu kuupäev:</b> {transaction.tradeDate}</p>
+                    <p><b>Tehingu avalikustamise kuupäev:</b> {transaction.publishedDate}</p>
+                    <p><b>Tehingu koht:</b> {transaction.market}</p>
+                    <p><b>Algne teade / muudatus:</b> {transaction.hasBeenUpdated ? 'Muudatus' : 'Algne teade'}</p>
+                    {transaction.updateReason !== '' && <p><b>Muudatuse põhjus:</b> {transaction.updateReason}</p>}
+                </div>
+            </div>
+        </main >
     );
 };
 
